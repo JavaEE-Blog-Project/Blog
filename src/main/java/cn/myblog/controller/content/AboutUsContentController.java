496aae6e4bff47541e259183c805f5932c9e5c4e
Joy Guo Guohuan <joykuo19991020@vip.qq.com>
2019-12-17 21:33:29 +0800

    AboutUs Page

---
+++ src/main/java/cn/myblog/controller/content/AboutUsContentController.java
@@ -0,0 +1,14 @@
+package cn.myblog.controller.content;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/AboutUs")
+public class AboutUsContentController {
+    @GetMapping
+    public String aboutUs(){
+        return "AboutUs";
+    }
+}
